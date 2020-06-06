48bda5873d378543dee067399a04272a2281b0e6
Andrew-van-Campen <andrewvancampen@gmail.com>
2020-06-05 22:39:46 -0400

    Changes to previous edits

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 //This code handles most general datalogger functionality.
 
-#include "help.h"
 #include "settings.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,7 +23,7 @@ int main(int argc, char **argv)
     //In the case of no command entered, ask for a command.
     if (argc == 1)
     {
-        printf("No command entered. Type ./dl followed by a command.\n");
+        printf("No command entered. Type ./datalogger followed by a command.\n");
         return 0;
     }
 
@@ -47,10 +46,6 @@ int main(int argc, char **argv)
         {
             printf("ERROR: Not a recognized setting.\n");
         }
-    }
-    else if (strcmp(*(argv + 1), "help") == 0)
-    {
-        help();
     }
     else
     {
